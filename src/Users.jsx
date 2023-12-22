415163f641832c52d5743e4b82d21d56be397cb8
Kumar Anuj <anujkumar.credencetech@gmail.com>
2023-12-22 18:58:40 +0530

    added comment filed in user add

--- src/Users.jsx
+++ src/Users.jsx
@@ -3,6 +3,7 @@ import toast from 'react-hot-toast';
 import { devUrl, useLocalStorage } from './utils'
 import { useNavigate } from 'react-router-dom'
 import image from './assets/image.png'
+import Tooltip from './components/Tooltip';
 
 const Users = () => {
     const [modelOpen, setModelOpen] = useState(false);
@@ -28,6 +29,7 @@ const Users = () => {
         name: particularItem ? particularItem?.name : '',
         account_number: particularItem ? particularItem?.account_number : '',
         date_time: particularItem ? dateFoemate(particularItem?.date_time) : '',
+        comment: particularItem ? particularItem?.comment : ''
     });
     const [currentPage, setCurrentPage] = useState(1);
     const [itemsPerPage, setItemsPerPage] = useState(10);
@@ -102,7 +104,8 @@ const Users = () => {
             setFormData({
                 name: '',
                 account_number: '',
-                date_time: ''
+                date_time: '',
+                comment: ''
             });
             setDefalutDateValue('')
             setParticularItem(null);
@@ -131,7 +134,8 @@ const Users = () => {
             setFormData({
                 name: '',
                 account_number: '',
-                date_time: ''
+                date_time: '',
+                comment: ''
             });
             setDefalutDateValue('')
             fetchUsers({ search: keywordValue, pageNumber: currentPage, pageSize: itemsPerPage });
@@ -250,7 +254,7 @@ const Users = () => {
         e.preventDefault();
 
         // Your form submission logic here
-        if (!formData.name || !formData.account_number || !defalutDateValue) {
+        if (!formData.name || !formData.account_number || !defalutDateValue || !formData?.comment) {
             setError('All fields are required');
             return;
         }
@@ -266,7 +270,8 @@ const Users = () => {
                     name: formData.name,
                     user_id: particularItem?.id,
                     account_number: formData.account_number,
-                    date_time: defalutDateValue
+                    date_time: defalutDateValue,
+                    comment: formData.comment
                 }
                 console.log("first body update", body)
                 updateUser(body)
@@ -274,8 +279,11 @@ const Users = () => {
                 const body = {
                     name: formData.name,
                     account_number: formData.account_number,
-                    date_time: defalutDateValue
+                    date_time: defalutDateValue,
+                    comment: formData.comment
                 }
+
+                console.log("first body add", body)
                 if (body) {
                     addUser(body)
                 }
@@ -437,6 +445,18 @@ const Users = () => {
                                         className="w-full  bg-opacity-40 rounded border bg-slate-700 border-gray-700 focus:ring-2 focus:ring-indigo-300  focus:border-indigo-500 text-base outline-none py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                                     />
                                 </div>
+                                <div className="relative sm:mr-4 mt-3 mr-2">
+                                    <label className="leading-7 text-sm text-gray-400" htmlFor="comment">
+                                        Comment
+                                    </label>
+                                    <textarea
+                                        name="comment"
+                                        id="comment"
+                                        value={formData.comment}
+                                        onChange={handleChange}
+                                        className="w-full  bg-opacity-40 rounded border bg-slate-700 border-gray-700 focus:ring-2 focus:ring-indigo-300  focus:border-indigo-500 text-base outline-none py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                                    />
+                                </div>
 
 
                                 <hr className='my-6' />
@@ -471,10 +491,10 @@ const Users = () => {
                                             <div className="font-semibold text-center">Date</div>
                                         </th>
                                         <th className="p-2">
-                                            <div className="font-semibold text-center">Edit</div>
+                                            <div className="font-semibold text-left">Comment</div>
                                         </th>
                                         <th className="p-2">
-                                            <div className="font-semibold text-center">Delete</div>
+                                            <div className="font-semibold text-center"></div>
                                         </th>
                                     </tr>
                                 </thead>
@@ -495,14 +515,21 @@ const Users = () => {
                                                 <td className="p-2 whitespace-nowrap">
                                                     <div className="text-center text-emerald-500">{formatDate(item?.date_time)}</div>
                                                 </td>
-                                                <td className="p-2 whitespace-nowrap" >
+                                                <td className="p-2 " >
+                                                    {item?.comment && item?.comment.length > 25 ? (
+                                                        <Tooltip text={item?.comment} >
+                                                            <div className="text-left text-sky-500">{item?.comment.slice(0, 25)}...</div>
+                                                        </Tooltip>
+                                                    ) : (
+                                                        <div className="text-left text-sky-500">{item?.comment}</div>
+                                                    )}
+                                                </td>
+                                                <td className="p-2 whitespace-nowrap flex flex-row">
                                                     <div className="text-center">
                                                         <button onClick={() => { handleEdit(item) }} className="px-2 py-2  mr-2 font-semibold rounded bg-green-500  text-white">
                                                             <svg xmlns="http://www.w3.org/2000/svg" height="1.5em" fill='#2a2b2c' viewBox="0 0 512 512"><path d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z" /></svg>
                                                         </button>
                                                     </div>
-                                                </td>
-                                                <td className="p-2 whitespace-nowrap">
                                                     <div className="text-center ">
                                                         <button type="button" onClick={() => { onDeleteClick(item) }} className="px-2 py-2 font-semibold rounded bg-red-500  text-white">
                                                             <svg xmlns="http://www.w3.org/2000/svg" height="1.5em" fill='#2a2b2c' viewBox="0 0 448 512"><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z" /></svg>
