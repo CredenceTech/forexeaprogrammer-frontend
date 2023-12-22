9d034391c3bc3e0b345cafd20b13bc63f3b24091
Kumar Anuj <anujkumar.credencetech@gmail.com>
2023-12-22 15:02:18 +0530

    made date update perfect

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,7 +2,7 @@ import React, { useRef } from 'react';
 import toast from 'react-hot-toast';
 import { useLocalStorage } from '../utils';
 import { useNavigate } from 'react-router-dom'
-
+import image from '../assets/image.png'
 const Dashboard = () => {
     const textAreaRef = useRef(null);
     const textInputRef = useRef(null);
@@ -42,9 +42,7 @@ const Dashboard = () => {
             <header className="text-gray-400 bg-gray-500 body-font">
                 <div className="container mx-auto flex flex-wrap p-5 justify-between md:flex-row items-center">
                     <a href='/' className="flex title-font font-medium items-center text-white mb-4 md:mb-0">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
-                            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-                        </svg>
+                        <img height={40} width={40} className='rounded-full' src={image} alt="logo" />
                         <span className="ml-3 text-xl">{name?.email}</span>
                     </a>
                     <button onClick={() => { logout() }} className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">Log Out
